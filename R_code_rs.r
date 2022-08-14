d1fc38e4ce80634c14069d53787aafc90679ad1d
giuliacapasso <111195677+giuliacapasso@users.noreply.github.com>
2022-08-14 22:31:53 +0200

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,18 @@
 # Questo è il primo script che useremo a lezione
 
 library(raster)
+
+# Settaggio cartella di lavoro
+# setwd("/Users/giulia/scrivania/lab")
+
+# import
+l2011 <- brick("p224r63_2011.grd")
+l2011
+
+# plot
+plot(l2011)
+
+# https://www.r-graph-gallery.com/42-colors-names.html
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(l2011, col=cl)
+
